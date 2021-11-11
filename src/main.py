25b6f117be63014226f15c667c1e97092e739da6
Euan Reid <gitlab@euanreid.com>
2021-11-11 06:05:58 +0000

    Templates with boilerplate and 404

--- src/main.py
+++ src/main.py
@@ -3,18 +3,33 @@ Runs the app
 """
 from pathlib import Path
 
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, status
+from fastapi.exception_handlers import http_exception_handler
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from starlette.exceptions import HTTPException as StarletteHTTPException
 
 
-app = FastAPI(default_response_class=HTMLResponse)
+app = FastAPI(default_response_class=HTMLResponse, openapi_url=None)
 
 template_path = Path(__file__).resolve().parent / 'html'
 templates = Jinja2Templates(directory=str(template_path))
 
+
+@app.exception_handler(StarletteHTTPException)
+async def http_exception_handler(request: Request, exc: StarletteHTTPException) -> HTMLResponse:
+    if exc.status_code == status.HTTP_404_NOT_FOUND:
+        return templates.TemplateResponse(
+            name='404.html',
+            context={'request': request},
+            status_code=status.HTTP_404_NOT_FOUND
+        )
+    else:
+        return await http_exception_handler(request, exc)
+
+
 @app.get('/')
-async def home(request: Request):
+async def home(request: Request) -> HTMLResponse:
     """
     Shows the homepage
     """
