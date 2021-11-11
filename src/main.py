731e32fa3f816dfc46b1cd4860c52cd849b5676d
Euan Reid <gitlab@euanreid.com>
2021-11-11 05:07:53 +0000

    Make something that runs

--- src/main.py
+++ src/main.py
@@ -1,4 +1,24 @@
 """
 It'll do stuff (later)
 """
-pass
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI(default_response_class=HTMLResponse)
+
+@app.get('/')
+async def home():
+    """
+    Shows the homepage
+    """
+    return '''
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <title>Page!</title>
+        </head>
+        <body>
+            <p>Test!</p>
+        </body>
+    </html>
+    '''
