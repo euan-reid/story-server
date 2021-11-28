9c99feab6a64926724f48dd8981ea8270715a377
Euan Reid <gitlab@euanreid.com>
2021-11-28 02:11:03 +0000

    Return author by name, use models better

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@
 Runs the app
 """
 from pathlib import Path
+from typing import Type
 
 from fastapi import FastAPI, Request, status
 from fastapi.exception_handlers import http_exception_handler
@@ -11,13 +12,29 @@ from google.cloud import datastore
 from pydantic.types import UUID4
 from starlette.exceptions import HTTPException as StarletteHTTPException
 
-from models import categories_literal, DatastoreModel
+import models
 
 client = datastore.Client()
 app = FastAPI(default_response_class=HTMLResponse, openapi_url=None)
 
 template_path = Path(__file__).resolve().parent / 'html'
 templates = Jinja2Templates(directory=str(template_path))
+
+
+def format_template(
+    request: Request,
+    page_title: str,
+    category: str,
+    resource: Type[models.DatastoreModel]
+) -> templates.TemplateResponse:
+    return templates.TemplateResponse(
+        name=f'{category}.html',
+        context={
+            'page_title': page_title,
+            'request': request,
+            'resource': resource
+        }
+    )
 
 
 @app.exception_handler(StarletteHTTPException)
@@ -49,16 +66,26 @@ async def home(request: Request) -> HTMLResponse:
     )
 
 
+@app.get('/authorbyname/{name}', response_model=models.Author)
+async def author_by_name(request: Request, name: str) -> models.Author:
+    author = models.Author.from_name(name)
+
+    if author is None:
+        raise StarletteHTTPException(status_code=status.HTTP_404_NOT_FOUND)
+
+    return format_template(request, author.name, 'author', author)
+
+
 @app.get('/{category}/{item_id}')
 async def page(
     request: Request,
-    category: categories_literal,
+    category: models.categories_literal,
     item_id: UUID4
 ) -> HTMLResponse:
     """
     Shows a page
     """
-    resource = DatastoreModel.from_type_and_id(
+    resource = models.DatastoreModel.from_type_and_id(
         subclass_name=category,
         id=item_id
     )
@@ -66,7 +93,4 @@ async def page(
     if resource is None:
         raise StarletteHTTPException(status_code=status.HTTP_404_NOT_FOUND)
 
-    return templates.TemplateResponse(
-        name=f'{category}.html',
-        context={'request': request, 'id': item_id, 'resource': resource}
-    )
+    return format_template(request, resource.name, category, resource)
